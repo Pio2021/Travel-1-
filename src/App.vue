1648dc96e25523cc34bee113cdc1c7a0afa5d74b
Pio2021 <1253835456@qq.com>
2021-09-06 14:48:23 +0800

    提交文件

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <keep-alive exclude="Detail">    <!-- exclude 在这里是不被缓存，后面是组件名字 -->
+      <router-view/>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
